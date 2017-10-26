7a2a4dcee65e0ee4ad78ad9e780237a5e9e7affa
fengyuz <fengyuz@umich.edu>
2017-10-26 09:29:09 -0400

    Add files via upload

---
+++ fy/COPY_files_on_CRANIUM_NEW.R
@@ -0,0 +1,77 @@
+#!/usr/bin/env Rscript
+
+dir.create("~/fengyu/")
+#w0<-list.files(pattern ="\*_ADNI_dataset_MN.RData$")
+# print(w0)
+#w10<-list.files(pattern = "*.RData$", full.names = TRUE)
+#w0<-list.files(pattern = "*_ABIDE_dataset.RData$", full.names = TRUE)
+file.copy("/ifshome/pipelnvt/CBDA_Inference_Binomial_dataset_3_final_INFERENCE.RData",
+"~/fengyu/CBDA_Inference_Binomial_dataset_3_final_INFERENCE.RData")
+file.copy("/ifshome/pipelnvt/CBDA_Inference_Binomial_dataset_3_final_INFERENCEqa_ALL.RData",
+          "~/fengyu/CBDA_Inference_Binomial_dataset_3_final_INFERENCEqa_ALL.RData")
+#CBDA_Inference_Binomial_dataset_3_final_INFERENCEqa_ALL.RData
+# file.copy("Data_Consolidation_MSE_FeatureMining_V16_Binomial_new_asRscript.R",
+#           "/ifshome/pipelnv/Data_Consolidation_MSE_FeatureMining_V16_Binomial_new_asRscript.R")
+# file.copy("CBDA_SL_M9000_miss0_n55_65_k5_10_Light_266_ABIDE_dataset.RData",
+#           "CBDA_SL_M9000_miss0_n55_65_k5_10_Light_265_ABIDE_dataset.RData")
+
+
+#w20<-list.files(pattern = "*_9000_ADNI_dataset_MN4.RData$", full.names = TRUE)
+#w00<-list.files(pattern = "*_Light_ADNI_dataset_MN3.RData$", full.names = TRUE)
+#print(length(w20))
+#print(length(w0))
+#print(w0)
+#print(length(w00))
+#file.remove(w0)
+#file.remove(w00)
+# print(w00)
+# file.copy(w00,"~/ADNI-MN4/")
+# w000<-list.files("~/ADNI-MN4/")
+# print(length(w000))
+# print(w000)
+#  Sys.chmod(list.dirs("/ifshome/pipelnvt/Dec6/*"), "777")
+#  unlink("/ifshome/pipelnvt/Dec6", force = TRUE)
+#  #file.remove(w0)
+#  w0<-list.files("/ifshome/pipelnvt/Dec6/")
+#  print(length(w0))
+
+# w0<-list.files()
+# print(length(w0))
+# file.remove(w0)
+# w0<-list.files()
+# print(length(w0))
+# w00<-list.files(pattern = "\\RData$", full.names = TRUE)
+# # w10<-list.files(pattern = "\\_Light_Gaussian_Dec5.RData$", full.names = TRUE)
+# # w100<-list.files(pattern = "\\_Light_Random_Dec5.RData$", full.names = TRUE)
+# print(length(w00))
+# file.remove(w00)
+# w00<-list.files(pattern = "\\CBDA_SL_M9000_miss20_n60_80_k5_15_Light_9_NULL_dataset.RData$", full.names = TRUE)
+# # w10<-list.files(pattern = "\\_Light_Gaussian_Dec5.RData$", full.names = TRUE)
+# # w100<-list.files(pattern = "\\_Light_Random_Dec5.RData$", full.names = TRUE)
+# print(length(w00))
+
+# print(length(w10))
+# print(length(w00))
+# print(length(w100))
+# dir.create("/ifshome/pipelnvt/GaussianNEW/")
+# file.copy(w00,"/ifshome/pipelnvt/Binomial9000/")
+# w000<-list.files("/ifshome/pipelnvt/Binomial9000/")
+# print(length(w000))
+# print(w000)
+# # file.info(w10)['mtime']
+# # file.info(w100)['mtime']
+#file.mtime(w10)
+#  w00<-list.files(pattern = "\\_3000_Gaussian_Dec5.RData$", full.names = TRUE)
+  # # w00<-list.files(pattern = "\\_3000_Binomial_Dec5.RData$", full.names = TRUE)
+# # print(length(w00))
+#   print(w0)
+# #print(w00)
+# # file.info(w0)['size']
+# # file.info(w00)['size']
+# # # print(w10)
+# # # file.info(w0)['size']
+#   file.copy(w0,"/ifshome/pipelnvt/Gaussian/")
+# # # # file.copy(w00,"/ifshome/pipelnvt/Nov32/")
+#    w10<-list.files("/ifshome/pipelnvt/Gaussian/")
+#     print(length(w10))
+#    print(w10)
